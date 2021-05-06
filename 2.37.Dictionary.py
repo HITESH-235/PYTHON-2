c2e01abaa23bcaaa557610be0c78cc623f80593c
HITESH-235 <sinhahitesh0307@gmail.com>
2021-05-06 09:04:32 -0800

    Update 2.37.Dictionary.py

--- 2.37.Dictionary.py
+++ 2.37.Dictionary.py
@@ -16,3 +16,35 @@ for z in y:
 
 print('Letter "a" has',d['a'],'occurences')
 print('Letter "h" has',d['h'],'occurences')
+
+x.close()
+
+#now we will write code to calculate points in scrabble game
+#Suppose there is 10 points on letter 'a' and 20 points on letter 'h'
+
+x1 = open(r'2.35.Text_File.txt','r')
+
+y1 = x1.read()
+
+d1 = {}
+
+for z1 in y1:
+
+    if z1 not in d1:
+        d1[z1] = 0
+
+    d1[z1]+= 1
+
+d2 = {'a': 10, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f':4, 'g': 2, 'h':20, 'i':1, 'j':8, 'k':5, 'l':1, 'm':3, 'n':1, 'o':1, 'p':3, 'q':10, 'r':1, 's':1, 't':1, 'u':1, 'v':4, 'w':4, 'x':8, 'y':4, 'z':10}
+
+a = 0
+
+for z2 in d1:
+
+    if z2 in d2:
+        a+= (d1[z2]*d2[z2])
+
+print(a)
+
+x1.close()
+
